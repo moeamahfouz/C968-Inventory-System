6abb2b4512161eb06e9d1bc4ef9713fcdb29dee4
moeamahfouz <akitaman19@gmail.com>
2022-12-11 19:47:54 -0500

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Mohamed_Mahfouz_Inventory_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
